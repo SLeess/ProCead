1157fc93e514c2d926880a7702b2b8eaf62ed157
Lean <duraesleandro21@gmail.com>
2025-07-10 11:39:35 -0300

    ✨ feat: Paginação e filtros de busca de MeusProcessos, além de texto template para caso não haja nenhum item

--- frontend/src/Pages/Candidato/MeusProcessos/MeusProcessos.jsx
+++ frontend/src/Pages/Candidato/MeusProcessos/MeusProcessos.jsx
@@ -1,47 +1,211 @@
-import React from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './MeusProcessos.css'
 import { FaSearch } from 'react-icons/fa';
 import { IoIosFunnel } from "react-icons/io";
+import { CiInboxIn } from "react-icons/ci";
 import ProcessoPessoalCard from '@/Components/Candidatos/ProcessoPessoalCard/ProcessoPessoalCard.tsx';
-
-const processosData = [
-  {
-    id: 102,
-    edital: 'EDITAL N.° 08/2025',
-    descricao: 'Processo de Seleção de Discentes para os Cursos de Especialização da Unimontes Modalidade Educação a Distância',
-    status: "Em andamento",
-    obs: "",
-  },
-  {
-    id: 104,
-    edital: 'EDITAL N.° 11/2024',
-    descricao: 'Processo de Seleção de Discentes para os Cursos de Especialização da Unimontes Modalidade Educação a DistânciaSeleção para o Programa de Pós-Graduação em Biotecnologia (PPGBiotec)',
-    status: "Em andamento",
-    obs: "",
-  },
-  {
-    id: 106,
-    edital: 'EDITAL N.° 01/2025',
-    descricao: 'Seleção de tutores para cursos de Educação a Distância (EaD)',
-    status: "Encerrado",
-    obs: "",
-  },
-  {
-    id: 101,
-    edital: 'EDITAL N.° 12/2024',
-    descricao: 'Seleção para Mestrado em Produção Vegetal no Semiárido',
-    status: "Encerrado",
-    obs: "",
-  },
-];
+import { getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table';
+import { useAppContext } from '@/Contexts/AppContext';
+import { toast } from 'react-toastify';
+import LoaderPages from '@/Components/Global/LoaderPages/LoaderPages';
 
 function MeusProcessos() {
+  const { token } = useAppContext();
+  const [meusProcessos, setMeusProcessos] = useState([]);
+  const [search, setSearch] = useState('');
+  
+  /** ------------------ Lidando com Filtros ------------------ **/
+  const [filtro, setFiltro] = useState("Todos");
+  const [filterOpen, setFilterOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState([]);
+  const filterDropdownRef  = useRef(null);
+
+  /** Buscar na API os processos seletivos que o usuário participa ou participou **/
+
+  useEffect(() => {
+    const fetchMeusProcessos = async () => {
+      setLoading(true);
+      setErrors([]);
+      /** pra testar o loading com demora */
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      try {
+          const res = await fetch('/api/user/meus-processos', { // Substitua pela URL real da sua API
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`, // Descomente se precisar de token
+              },
+          });
+
+          if (!res.ok) {
+              throw new Error(`Erro ao buscar processos: ${res.status} ${res.statusText}`);
+          }
+
+          const result = await res.json();
+          setMeusProcessos(result.data);
+      } catch (err) {
+          setErrors(e => [...e, "Não foi possível carregar seus processos seletivos. Tente novamente mais tarde.", err]);
+          setMeusProcessos([]);
+      } finally {
+        setLoading(false);
+        errors.forEach((error) => toast.error(error));
+      }
+    };
+    fetchMeusProcessos();
+  }, []);
+
+  /**Atualizar e aparecer o dropdown de filtros */
+  useEffect(() => {
+      if (!filterOpen) return;
+
+      const handleClickOutside = (event) => {
+          if (filterDropdownRef.current && !filterDropdownRef.current.contains(event.target)) {
+              setFilterOpen(false);
+          }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+      };
+  }, [filterOpen]);
+
+  function handleChangeFilters(e){
+    setFiltro(e.target.value);
+  }
+
+  /** --------------------------------------------------------- **/
+
+  /** ------------------ Lidando com Tabelação dos componentes cards para permitir paginação ------------------ **/
+  const [pagination, setPagination] = useState({
+      pageIndex: 0,
+      pageSize: 5,
+  });
+  const data = useMemo(() => meusProcessos, [meusProcessos]);
+  const columns = useMemo(() => [
+      { accessorKey: 'descricao', header: 'Descrição', cell: info => info.getValue() },
+      { accessorKey: 'edital', header: 'Edital', cell: info => info.getValue() },
+      { accessorKey: 'status', header: 'Status', cell: info => info.getValue() },
+      { accessorKey: 'obs', header: 'Observação', cell: info => info.getValue() },
+  ], []);
+
+  const table = useReactTable({
+      data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+      getPaginationRowModel: getPaginationRowModel(),
+      getFilteredRowModel: getFilteredRowModel(),
+      state: {
+          globalFilter: search,
+          pagination,
+      },
+      onGlobalFilterChange: setFiltro,
+      onPaginationChange: setPagination,
+      // Adiciona um filtro de coluna personalizado pelo status e pelo campo de Pesquisa
+      globalFilterFn: (row, columnId, filterValue) => {
+          // Primeiro, o filtro de busca global (se houver um valor de busca)
+          var infoReturn = false;
+          if (search) {
+              const cellValue = row.getValue(columnId);
+              if (typeof cellValue === 'string' && cellValue.toLowerCase().includes(filterValue.toLowerCase())) {
+                  infoReturn = true;
+              }
+          }
+          // Se não houver busca global, ou se a busca global passou, aplique o filtro de status
+          if (filtro && (filtro === row.original.status)) {
+              infoReturn = false;
+          }
+          return infoReturn;
+      },
+  });
+
+  const getFilteredAndPaginatedRows = () => {
+      const rows = table.getRowModel().rows;
+
+      if (filtro === 'Todos') {
+          return rows;
+      }
+      return rows.filter(row => {
+        return filtro === row.original.status;
+      });
+  };
+
+  const paginatedAndFilteredRows = getFilteredAndPaginatedRows();
+
+
+  const renderPageNumbers = () => {
+    const totalPages = table.getPageCount();
+    const currentPage = table.getState().pagination.pageIndex;
+    const pageNumbers = [];
+    const maxPagesToShow = 7; // Quantidade máxima de botões de página para exibir
+
+    if (totalPages <= maxPagesToShow) {
+        for (let i = 0; i < totalPages; i++) {
+            pageNumbers.push(i);
+        }
+    } else {
+        // Lógica para mostrar ellipsis se houver muitas páginas
+        let startPage = Math.max(0, currentPage - Math.floor(maxPagesToShow / 2));
+        let endPage = Math.min(totalPages - 1, currentPage + Math.floor(maxPagesToShow / 2));
+
+        if (endPage - startPage + 1 < maxPagesToShow) {
+            if (currentPage < Math.floor(maxPagesToShow / 2)) {
+                endPage = maxPagesToShow - 1;
+            } else if (currentPage > totalPages - 1 - Math.floor(maxPagesToShow / 2)) {
+                startPage = totalPages - maxPagesToShow;
+            }
+        }
+
+        if (startPage > 0) {
+            pageNumbers.push(0);
+            if (startPage > 1) pageNumbers.push('...');
+        }
+
+        for (let i = startPage; i <= endPage; i++) {
+            pageNumbers.push(i);
+        }
+
+        if (endPage < totalPages - 1) {
+            if (endPage < totalPages - 2) pageNumbers.push('...');
+            pageNumbers.push(totalPages - 1);
+        }
+    }
+    return pageNumbers.map((page, index) => (
+        <React.Fragment key={index}>
+            {page === '...' ? (
+                <span className="mx-3 my-1 text-gray-500">...</span>
+            ) : (
+                <button
+                    onClick={() => table.setPageIndex(page)}
+                    className={`px-3 py-2 mx-1 hover:cursor-pointer text-md not-italic font-bold leading-[18px] ${
+                        table.getState().pagination.pageIndex === page
+                            ? 'text-[#004DA9]' // página ativa
+                            : 'text-black hover:bg-gray-200 ' // páginas inativas
+                    }`}
+                >
+                    {page + 1}
+                </button>
+            )}
+        </React.Fragment>
+    ));
+};
+  /** --------------------------------------------------------------------------------------------------------- **/
+
+  function handleFormSubmit(e){
+    e.preventDefault();
+  }
+
   return (
-    <section className='mx-auto px-2 md:px-6 mt-10 py-12 bg-white rounded-2xl shadow-[-1px_0px_2px_0px_rgba(0,0,0,0.25),1px_1px_2px_0px_rgba(0,0,0,0.25)]'>
-      <form className='max-w-5xl mx-auto' onSubmit={() => {}}>
+    <>
+    {/* {
+        loading &&
+        <LoaderPages/>
+    } */}
+    <section className='mx-auto min-h-[75vh] px-2 md:px-6 mt-10 py-12 bg-white rounded-2xl shadow-[-1px_0px_2px_0px_rgba(0,0,0,0.25),1px_1px_2px_0px_rgba(0,0,0,0.25)]'>
+      <form className='max-w-5xl mx-auto' onSubmit={handleFormSubmit}>
         <header className="flex flex-col md:flex-row items-center md:justify-between mb-12 mx-2 space-y-4 md:space-y-0">
           <h1 className="text-3xl text-center sm:text-4xl text-gray-800 font-normal">Meus Processos Seletivos</h1>
-          <div className='flex flex-col sm:flex-row gap-2 items-center sm:items-baseline'>
+          <div className='flex flex-col items-center sm:flex-row gap-2'>
             <div className="relative max-w-[250px] md:max-w-[350px] w-full mx-0 md:mx-2">
               <div className="absolute top-3 left-0 flex items-center pl-3 pointer-events-none">
                 <FaSearch className="text-gray-400" />
@@ -49,31 +213,108 @@ function MeusProcessos() {
               <input
                 type="text"
                 placeholder="Pesquisar..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
                 className="h-[40px] w-full md:w-[350px] p-2 pl-10 text-sm text-gray-900 border-solid border-[rgba(0,0,0,0.50)] border-[0.4px] rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
               />
               <button className='absolute top-0 right-0 flex items-center bg-[#095ec5] hover:bg-[#0751ac] text-white h-[40px] p-[10px_12px] justify-center rounded-r-lg'>
                 Buscar
               </button>
             </div>
-            <button className='h-[40px] max-w-30 hover:cursor-pointer inline-flex justify-center items-center gap-3 justify-items-end sm:justify-items-normal bg-[white] shadow-[0px_0px_0px_1px_rgba(0,0,0,0.08),0px_6px_24px_0px_rgba(0,0,0,0.05)] pt-[11px] pb-2.5 px-[15px] rounded-[10px]'>
-              <IoIosFunnel/>
-              Filtros
-            </button>
+            <div className='' ref={filterDropdownRef}>
+              <button 
+                  className={`h-[40px] max-w-30 
+                    hover:cursor-pointer 
+                    inline-flex items-center gap-3 justify-items-end sm:justify-items-normal 
+                    bg-[white] shadow-[0px_0px_0px_1px_rgba(0,0,0,0.08),0px_6px_24px_0px_rgba(0,0,0,0.05)] 
+                    pt-[11px] pb-2.5 px-[15px] rounded-[10px]
+                    focus:border-2
+                  `}
+                  onClick={() => setFilterOpen(!filterOpen)}
+              >
+                <IoIosFunnel/>
+                Filtros
+              </button>
+              {filterOpen && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-700 rounded-md shadow-lg z-10 p-2">
+                      <p className="text-sm font-semibold text-gray-800 dark:text-white mb-2">Filtrar por Status</p>
+                      {['Todos', 'Em andamento', 'Encerrado'].map(statusOption => (
+                          <div key={statusOption} className="flex items-center mb-1">
+                              <input
+                                  type="radio"
+                                  id={`status-${statusOption}`}
+                                  name="statusFilter"
+                                  value={statusOption}
+                                  checked={filtro === statusOption}
+                                  onChange={handleChangeFilters}
+                                  className="form-radio text-blue-600"
+                              />
+                              <label htmlFor={`status-${statusOption}`} className="ml-2 text-sm text-gray-700 dark:text-gray-200">
+                                  {statusOption}
+                              </label>
+                          </div>
+                      ))}
+                  </div>
+                )}
+            </div>
+
           </div>
         </header>
-          <ul className='grid grid-cols-1 gap-6'>
-          {/* <ul className='space-y-5 row'> */}
-            {
-              processosData.map((processo) => 
-                  <ProcessoPessoalCard 
-                    key={processo.id} 
-                    processo={processo}
-                    />
+        {
+        loading ?
+            <LoaderPages/> : 
+          paginatedAndFilteredRows.length > 0 ?
+              (           
+                <ul className='grid grid-cols-1 gap-6'>
+                  {
+                    paginatedAndFilteredRows.map((processo) => 
+                        <ProcessoPessoalCard 
+                          key={processo.original.id} 
+                          processo={processo.original}
+                          />
+                    )
+                  }
+                </ul>
               )
-            }
-          </ul>
+            :
+            <div className='flex flex-col items-center justify-center pb-25 text-gray-500 dark:text-gray-400'>
+                <CiInboxIn className="w-24 h-24 mb-4 text-gray-400 dark:text-gray-500" />
+                <p className="text-xl font-semibold mb-2">Ops! Nenhum processo foi encontrado.</p>
+                <p className="text-base text-center max-w-md">
+                    Não encontramos nenhum processo seletivo que corresponda aos seus critérios de busca ou filtros os quais você tenha participado.
+                    Tente ajustar sua pesquisa ou limpar os filtros para ver mais opções.
+                </p>
+            </div>
+        }
+        {
+          paginatedAndFilteredRows.length > 0 && 
+            <div className="flex flex-col sm:flex-row justify-center items-center mt-8 px-2 md:px-0 text-sm">
+              <div className="flex items-center space-x-2 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.08),0px_6px_24px_0px_rgba(0,0,0,0.05)] bg-white">
+                  <button
+                      onClick={() => table.previousPage()}
+                      disabled={!table.getCanPreviousPage()}
+                      className="px-4 py-2 border border-gray-300 dark:border-gray-600 bg-[#F2F5F7] dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                  >
+                      &lt;
+                  </button>
+
+                  <div className="flex ">
+                      {renderPageNumbers()}
+                  </div>
+
+                  <button
+                      onClick={() => table.nextPage()}
+                      disabled={!table.getCanNextPage()}
+                      className="px-4 py-2 border border-gray-300 dark:border-gray-600 bg-[#F2F5F7] dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                  >
+                      &gt;
+                  </button>
+              </div>
+          </div>
+        }
       </form>
     </section>
+    </>
   );
 }
 
--- frontend/src/Pages/Candidato/ProcessosAtivos/ProcessosAtivos.jsx
+++ frontend/src/Pages/Candidato/ProcessosAtivos/ProcessosAtivos.jsx
@@ -1,9 +1,78 @@
-import Processos from "@/Components/Candidatos/Processos/Processos";
+import ProcessoCard from "@/Components/Candidatos/ProcessoCard/ProcessoCard";
+import { CiInboxIn } from "react-icons/ci";
+
+const processosData = [
+  {
+    id: 102,
+    edital: 'Edital 08/2025',
+    descricao: 'Processo Seletivo para contratação de Professor Substituto de Direito',
+    inscrito: true,
+  },
+  {
+    id: 104,
+    edital: 'Edital 11/2024',
+    descricao: 'Processo de Seleção de Discentes para os Cursos de Especialização da Unimontes Modalidade Educação a DistânciaSeleção para o Programa de Pós-Graduação em Biotecnologia (PPGBiotec)',
+    inscrito: true,
+  },
+  {
+    id: 106,
+    edital: 'Edital 01/2025',
+    descricao: 'Seleção de tutores para cursos de Educação a Distância (EaD)',
+    inscrito: true,
+  },
+  {
+    id: 101,
+    edital: 'Edital 12/2024',
+    descricao: 'Seleção para Mestrado em Produção Vegetal no Semiárido',
+    inscrito: false,
+  },
+  {
+    id: 103,
+    edital: 'Edital 02/2025',
+    descricao: 'Vagas remanescentes para o curso de Especialização em Gestão Pública',
+    inscrito: false,
+  },
+  {
+    id: 105,
+    edital: 'Edital 09/2025',
+    descricao: 'Processo de Seleção de Discentes para o curso de Odontologia',
+    inscrito: false,
+  },
+];
 
 export default function ProcessosAtivos(){
   return (
-    <>
-      <Processos /> 
-    </>
+    <section className="min-h-[75vh] px-6 mt-10 py-12 bg-white rounded-2xl" style={{boxShadow: "-1px 0px 2px 0px rgba(0, 0, 0, 0.25), 1px 1px 2px 0px rgba(0, 0, 0, 0.25)",}}>
+        <div className="text-center mb-12">
+          <h1 className="text-3xl sm:text-4xl text-gray-800 font-normal">Processos em Andamento</h1>
+          <p className="text-gray-600 mt-2">Confira aqui todos os processo em andamento atualmente.</p>
+        </div>
+
+        {/* Grade Responsiva: 
+            - 1 coluna em telas pequenas (padrão)
+            - 2 colunas em telas médias (md)
+            - 3 colunas em telas grandes (lg)
+        */}
+          {processosData.length > 0 ?
+          (           
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:mx-20">
+              {
+                processosData.map((processo) => (
+                  <ProcessoCard key={processo.id} processo={processo} />
+                ))
+              }
+            </div>
+          )
+          :
+            <div className='flex flex-col items-center justify-center pb-25 text-gray-500 dark:text-gray-400'>
+                <CiInboxIn className="w-24 h-24 mb-4 text-gray-400 dark:text-gray-500" />
+                <p className="text-xl font-semibold mb-2">Ops! Nenhum processo em andamento foi encontrado.</p>
+                <p className="text-base text-center max-w-md">
+                    Não encontramos nenhum processo seletivo que esteja em andamento hoje.
+                    Aguarde o início de novos processos.
+                </p>
+            </div>
+          }
+    </section>
   );
 };
