2bb662a57398787229c7c1679c069dc0d45afaa8
Lean <duraesleandro21@gmail.com>
2025-07-24 16:48:13 -0300

    ♻️ Refactor: Componentização do header de admin e candidato para reutilização de código

---
+++ frontend/src/Components/Global/HeaderModel/HeaderModel.jsx
@@ -0,0 +1,31 @@
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from "flowbite-react";
+import UserDropdown from "../UserDropdown/UserDropdown";
+import ThemeToggleBtn from "../ThemeToggleBtn/ThemeToggleBtn";
+
+export default function HeaderModel({ headerid, children }){
+    return(
+        <header id={headerid}>
+            <Navbar fluid rounded>
+                <NavbarBrand href="#" className="md:hidden block">
+                <img src={`/img/logo_cead_bg_white_full.png`} alt="Logo CEAD" className="w-[120px] h-[50px]"/>
+                </NavbarBrand>
+                <div className="hidden md:flex space-x-10 items-center">
+                <NavbarBrand href="#">
+                    <img src="/img/logo_cead_bg_white_full.png" alt="Logo CEAD" className="w-[120px] h-[50px]"/>
+                </NavbarBrand>
+                <NavbarCollapse className="space-x-10">
+                    { children }
+                </NavbarCollapse>
+                </div>
+                <div className="flex md:order-2 space-x-2">
+                <ThemeToggleBtn />
+                <UserDropdown/>
+                <NavbarToggle />
+                </div>
+                <NavbarCollapse className="md:hidden">
+                    { children }
+                </NavbarCollapse>
+            </Navbar>
+        </header>
+    );
+}
