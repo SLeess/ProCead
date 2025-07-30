44cf8f12adcd6b8c27d3168154bf2a7705757d70
Lean <duraesleandro21@gmail.com>
2025-07-30 13:46:00 -0300

    🐛 Bug fix: Problemas do vite com o arquivo .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,38 @@
 
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv  } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-  server: {
-    proxy:{
-      '/api': {
-        target: import.meta.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        headers: {
-          Accept: 'application/json',
-          "Content-Type": 'application/json',
-        },
+export default defineConfig(({mode}) => {
+
+  const currentDir = fileURLToPath(new URL('.', import.meta.url))
+
+  const env = loadEnv(mode, currentDir, 'VITE_');
+
+  return {
+    plugins: [react(), tailwindcss(), flowbiteReact()],
+    server: {
+      port: 3000,
+      proxy:{
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          headers: {
+            Accept: 'application/json',
+            "Content-Type": 'application/json',
+          },
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      // Aqui estamos dizendo ao Vite: sempre que encontrar '@' no início de um import,
-      // substitua por 'caminho/absoluto/do/projeto/src'
-      '@': fileURLToPath(new URL(import.meta.env.VITE_APP_BASE_URL, import.meta.url)),
     },
-  },
+    resolve: {
+      alias: {
+        // Aqui estamos dizendo ao Vite: sempre que encontrar '@' no início de um import,
+        // substitua por 'caminho/absoluto/do/projeto/src'
+        '@': fileURLToPath(new URL(env.VITE_APP_BASE_URL, import.meta.url)),
+      },
+    },
+  }
 })
