1f7e7e452b09d7b0db88309d107f90d2507a9626
Lean <duraesleandro21@gmail.com>
2025-07-30 13:51:21 -0300

    🧹cleanup: tabulação do header

--- frontend/src/Components/Global/HeaderModel/HeaderModel.jsx
+++ frontend/src/Components/Global/HeaderModel/HeaderModel.jsx
@@ -7,20 +7,20 @@ export default function HeaderModel({ headerid, children }){
         <header id={headerid}>
             <Navbar fluid rounded>
                 <NavbarBrand href="#" className="md:hidden block">
-                <img src={`/img/logo_cead_bg_white_full.png`} alt="Logo CEAD" className="w-[120px] h-[50px]"/>
+                    <img src={`/img/logo_cead_bg_white_full.png`} alt="Logo CEAD" className="w-[120px] h-[50px]"/>
                 </NavbarBrand>
                 <div className="hidden md:flex space-x-10 items-center">
-                <NavbarBrand href="#">
-                    <img src="/img/logo_cead_bg_white_full.png" alt="Logo CEAD" className="w-[120px] h-[50px]"/>
-                </NavbarBrand>
-                <NavbarCollapse className="space-x-10">
-                    { children }
-                </NavbarCollapse>
+                    <NavbarBrand href="#">
+                        <img src="/img/logo_cead_bg_white_full.png" alt="Logo CEAD" className="w-[120px] h-[50px]"/>
+                    </NavbarBrand>
+                    <NavbarCollapse className="space-x-10">
+                        { children }
+                    </NavbarCollapse>
                 </div>
                 <div className="flex md:order-2 space-x-2">
-                <ThemeToggleBtn />
-                <UserDropdown/>
-                <NavbarToggle className="text-white"/>
+                    <ThemeToggleBtn />
+                    <UserDropdown/>
+                    <NavbarToggle className="text-white"/>
                 </div>
                 <NavbarCollapse className="md:hidden">
                     { children }
