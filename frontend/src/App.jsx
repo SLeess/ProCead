e8c8430f0ea7afb2266ff0bfee21522fe272f3dc
Lean <duraesleandro21@gmail.com>
2025-08-08 14:34:58 -0300

    ✨feat: Relatórios custom page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,7 @@ import LayoutAdminInsideEdital from './Layouts/LayoutAdminInsideEdital';
     import GerenciarPerfisPermissoes from './Pages/Admin/Home/GerenciarPerfis/ActionPages/GerenciarPerfisPermissoes/GerenciarPerfisPermissoes';
   /** ------------------------------------ Páginas de Gerência de Permissões do Perfil ---------------------------- */
   import GerenciarUsuarios from './Pages/Admin/Home/GerenciarUsuarios/GerenciarUsuarios';
+  import RelatoriosCustom from './Pages/Admin/Home/RelatoriosCustom/RelatoriosCustom';
 
     /** ------------------------------------ Páginas Adm de Edital ---------------------------- */
     import Inscricoes from './Pages/Admin/InsideEdital/Inscricoes/Inscricoes';
@@ -137,6 +138,7 @@ function App() {
               <Route path='permissoes' element={<GerenciarPerfisPermissoes/>}></Route>
             </Route>
             <Route path="usuarios" element={<GerenciarUsuarios/>} />
+            <Route path="relatorios-custom" element={<RelatoriosCustom/>} />
             {/* <Route path='manejar-usuarios/:userId' element={<UserManagePage />} /> */}
           </Route>
 
