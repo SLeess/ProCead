126162c55ffb6c3fb301381253869270d5c2ab56
Lean <duraesleandro21@gmail.com>
2025-08-13 18:00:12 -0300

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,23 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-require __DIR__.'\api\guest.php';
+require __DIR__.'/api/guest.php';
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::middleware(['throttle:user'])->group(function(){
-        require __DIR__.'\api\auth.php';
+        require __DIR__.'/api/auth.php';
     });
 
     Route::middleware(['throttle:candidate'])->group(function(){
 
-        require __DIR__.'\api\candidate.php';
-        require __DIR__.'\api\inscricao.php';
+        require __DIR__.'/api/candidate.php';
+        require __DIR__.'/api/inscricao.php';
     });
 
     Route::middleware(['throttle:admin'])->group(function(){
-        require __DIR__.'\api\admin.php';
-        require __DIR__.'\api\super_admin.php';
+        require __DIR__.'/api/admin.php';
+        require __DIR__.'/api/super_admin.php';
     });
 });
