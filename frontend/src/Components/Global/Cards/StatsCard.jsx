51f5dab9a022f1258174f990f44429eae8c31e2f
Daniel Damasceno <dandamasceno04@gmail.com>
2025-08-14 16:19:33 -0300

    🧹cleanup: css separado para os cards

--- frontend/src/Components/Global/Cards/StatsCard.jsx
+++ frontend/src/Components/Global/Cards/StatsCard.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react'
 
 const StatsCard = ({title, quant, children}) => {
     return (
-        <div className="bg-[var(--admin-stats-card)] shadow-md rounded-md p-5 w-xs relative flex flex-col justify-between h-30">
-            <p className="text-[var(--admin-stats-card-text)] mb-1">{title}</p>
-            <p className="text-[var(--admin-stats-card-text)] text-2xl font-bold mb-1">{quant}</p>
+        <div id='card-container'>
+            <p id='card-title'>{title}</p>
+            <p id='card-quant'>{quant}</p>
             { children }
         </div>
     );
