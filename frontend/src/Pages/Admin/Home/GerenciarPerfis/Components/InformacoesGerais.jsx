a04606a51780ba8157eb6b1be7595c5c2e1cea39
Lean <duraesleandro21@gmail.com>
2025-08-18 13:52:43 -0300

    🚧in progress: Configuração de perfis e permissões

---
+++ frontend/src/Pages/Admin/Home/GerenciarPerfis/Components/InformacoesGerais.jsx
@@ -0,0 +1,39 @@
+import { FormField, SelectInput, TextInput } from "@/Components/Global/ui/modals";
+import { useMemo } from "react";
+
+export default function InformacoesGerais({role, setRole, readOnly = false})
+{
+    const selectedScopeLabel = useMemo(() => {
+        if (role.escopo === 'local') {
+            return 'Perfil Local';
+        }
+        if (role.escopo === 'global') {
+            return 'Perfil Global';
+        }
+        return '';
+    }, [role.escopo]);
+
+    const handleScopeChange = (e) => {
+        const selectedLabel = e.target.value;
+        const newScopeValue = selectedLabel === 'Perfil Local' ? 'local' : 'global';
+        setRole((prevRole) => ({ ...prevRole, escopo: newScopeValue }));
+    };
+
+    return (<>
+        <h2>Informações Gerais</h2>
+        <div className="grid grid-cols-12 sm:mt-3">
+            <FormField label="Nome do Perfil" className="mt-3 sm:mt-0 sm:mr-4">
+                <TextInput value={role.nome} onChange={(e) => setRole((p) => ({...p, nome: e.target.value}))} readOnly={readOnly}/>
+            </FormField>
+            <FormField label="Escopo do Perfil" className="mt-3 sm:mt-0">
+                <SelectInput
+                    value={selectedScopeLabel}
+                    onChange={handleScopeChange}
+                    readOnly={readOnly}
+                    options={['Perfil Local', 'Perfil Global']}
+                />
+            </FormField>
+        </div>
+        <hr className="my-7"/>
+    </>);
+}
