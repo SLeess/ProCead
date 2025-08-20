c696ffef772144f7c3ef1c768efb328941ca7879
Lean <duraesleandro21@gmail.com>
2025-08-20 16:53:15 -0300

    ✨feat: Tabelas adaptadas baseadas na tabela Main para simplificação de chamadas de tabelas parecidas em páginas diferentes com muitas adaptações

---
+++ frontend/src/Components/Global/Tables/TablePermissoesGlobais/TabelaPermissoesGlobais.jsx
@@ -0,0 +1,31 @@
+import React, { useMemo } from 'react';
+import MainTable from '@/Components/Global/Tables/MainTable/MainTable'; // Ajuste o caminho para seu MainTable
+import { TabelaPermissoesGlobaisGlobais } from './columns';
+// import { NavigationContext } from '@/Contexts/NavigationContext';
+
+export default function TabelaPermissoesGlobais({permissoesGlobais}) {
+  const data = useMemo(() => permissoesGlobais, [permissoesGlobais]);
+
+  // const { navigate } = useContext(NavigationContext);
+
+  const columns = useMemo(() => TabelaPermissoesGlobaisGlobais());
+  
+  return (
+    <div className="w-full md:w-md lg:w-lg  max-w-full rounded-lg">
+        <h2>Atribuir Permissões Globais</h2>
+        {/* <h3>As permissões atribuídas aos usuários através dos perfis globais valem independentemente do edital acessado.</h3> */}
+        <MainTable
+            data={data}
+            columns={columns}
+            hasSelectForRows={false}
+            hasCountSelectedLines={false}
+            canExport={false}
+            hasPaddingStyle={false}
+            hasShadowBorderStyle={false}
+            canHiddenColumns={false}
+            enableDataFooter={false}
+            className='border-0'
+        />
+    </div>
+  );
+}
