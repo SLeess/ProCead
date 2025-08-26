3ca3667a99cfc625202776a55fa75f6718831a73
Daniel Damasceno <dandamasceno04@gmail.com>
2025-08-26 16:12:53 -0300

    ✨feat: dados de modalidade e curso vindo da API para classificação

--- frontend/src/Pages/Admin/InsideEdital/Classificacao/Classificacao.jsx
+++ frontend/src/Pages/Admin/InsideEdital/Classificacao/Classificacao.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Classificacao.css";
 import dataPreview from "./dataPreview.js";
 import { Plus, Undo2 } from "lucide-react";
@@ -8,63 +8,126 @@ import { useParams } from "react-router-dom";
 import MainTable from "@/Components/Global/Tables/MainTable/MainTable";
 import columns from "./columns.jsx";
 import data from "./data.js";
+import { AppContext } from "@/Contexts/AppContext";
+import AccessDenied from "@/Components/Global/AccessDenied/AccessDenied";
 
 const Classificacao = () => {
-    const tabs = [
-        'Ampla Concorrência', 
-        'Negros e Pardos', 
-        'Pessoa com Deficiência', 
-        'Transgênero e Travesti', 
-        'Egresso de Escola Pública',
-        'Ver Todos'
-    ];
+    const [loading, setLoading] = useState([]);
+    const { editalId } = useParams();
+    const { hasPermissionForEdital, isSuperAdmin, token, verifyStatusRequest } = useContext(AppContext);
 
-    // filter by cursos.
+    // dados.
+    const [modalidades, setModalidades] = useState([]);
+    const [cursos, setCursos] = useState([]);
     const [selectedCursos, setSelectedCursos] = useState(null);
-    const cursos = [
-        { name: 'Especialização em Alfabetização e Multiletramentos' },
-        { name: 'Especialização em Arte e Cultura Visual' },
-        { name: 'Especialização em Apicultura' },
-        { name: 'Especialização em Artes Cênicas' },
-        { name: 'Especialização em Pecuária' },
-        { name: 'Especialização em Arqueologia' },
-    ];
 
-    return (
-        <div id="classificacao-content">
+    // fetch nas modalidades para as tabs.
+    async function fetchModalidades() {
+        try {
+            const res = await fetch(`/api/admin/modalidades/${editalId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
 
-            <div id="classificacao-header">
-                <p id="classificacao-title">Classificação</p>
-                <button id="aprovar-btn">
-                    <Plus className="inline" />
-                    <span className='ml-1 text-sm'>Aprovar Classificação</span>
-                </button>
+            if (!res.ok) {
+                verifyStatusRequest(res);
+                throw new Error(`Erro ao buscar processos: ${res.status} ${res.statusText}`);
+            }
+
+            const result = await res.json();
+            setModalidades(result.data);
+        } catch (error) {
+            setModalidades([]);
+            throw new Error(`Erro: ${error}`);
+        }
+    }
+
+    // fetch nos cursos para options do select.
+    async function fetchCursos() {
+        try {
+            const res = await fetch(`/api/admin/cursos/${editalId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (!res.ok) {
+                verifyStatusRequest(res);
+                throw new Error(`Erro ao buscar processos: ${res.status} ${res.statusText}`);
+            }
+
+            const result = await res.json();
+            setCursos(result.data);
+        } catch (error) {
+            setCursos([]);
+            throw new Error(`Erro: ${error}`);
+        }
+    }
+
+    // renderiza os dados no carregamento da página.
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                await Promise.all([fetchModalidades(), fetchCursos()]);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (hasPermissionForEdital('visualizar-modalidades', editalId) && hasPermissionForEdital('visualizar-cursos', editalId) || isSuperAdmin())
+
+        return (
+            <div id="classificacao-content">
+
+                <div id="classificacao-header">
+                    <p id="classificacao-title">Classificação</p>
+                    <button id="aprovar-btn">
+                        <Plus className="inline" />
+                        <span className='ml-1 text-sm'>Aprovar Classificação</span>
+                    </button>
+                </div>
+
+                <div className="card flex justify-content-center my-4">
+                    <MultiSelect 
+                        value={selectedCursos} 
+                        onChange={(e) => setSelectedCursos(e.value)} 
+                        options={cursos} 
+                        optionLabel="nome" 
+                        display="chip"
+                        placeholder="Selecione os Cursos" 
+                        className="w-[50%] md:w-20rem h-12 items-center" 
+                    />
+                </div>
+
+                <TabView scrollable >
+                    {modalidades.map((modalidade) => {
+                        return (
+                            <TabPanel className="text-sm" key={modalidade.id} header={modalidade.sigla}></TabPanel>
+                        );
+                    })}
+                </TabView>
+
+                <MainTable data = {data} columns = {columns} isClassificationTable={true} hasPaddingStyle={false} hasSelectForRows={false}/>
+
             </div>
+        );
 
-            <div className="card flex justify-content-center my-4">
-                <MultiSelect 
-                    value={selectedCursos} 
-                    onChange={(e) => setSelectedCursos(e.value)} 
-                    options={cursos} 
-                    optionLabel="name" 
-                    display="chip"
-                    placeholder="Selecione os Cursos" 
-                    className="w-[50%] md:w-20rem h-12 items-center" 
-                />
-            </div>
-
-            <TabView scrollable >
-                {tabs.map((tab) => {
-                    return (
-                        <TabPanel className="text-sm" key={tab} header={tab}></TabPanel>
-                    );
-                })}
-            </TabView>
-
-            <MainTable data = {data} columns = {columns} isClassificationTable={true} hasPaddingStyle={false} hasSelectForRows={false}/>
-
-        </div>
-    );
+    else {
+        return (
+            <AccessDenied />
+        )
+    }
 }
 
 export default Classificacao;
