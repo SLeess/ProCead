01ea1446e23d6a5eab6beb70d8fae5657a342686
Henrique <uni.hsilva@gmail.com>
2025-09-03 17:10:18 -0300

    ✨feat:  Página de inscrições para admin exibindo informações reais do banco. Falta modais de show e edit

--- frontend/src/Pages/Admin/InsideEdital/Inscricoes/Inscricoes.jsx
+++ frontend/src/Pages/Admin/InsideEdital/Inscricoes/Inscricoes.jsx
@@ -7,57 +7,63 @@ import columns from './columns';
 import { FileText } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 import StatsCard from '@/Components/Global/Cards/StatsCard';
+import LoaderPages from '@/Components/Global/LoaderPages/LoaderPages';
 
 const Inscricoes = () => {
   const { editalId } = useParams();
-  const { hasPermissionForEdital, isSuperAdmin, token, verifyStatusRequest} = useContext(AppContext);
+  const { hasPermissionForEdital, isSuperAdmin, token, verifyStatusRequest } = useContext(AppContext);
   const [inscricoes, setInscricoes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [needUpdate, setNeedUpdate] = useState(false);
 
   useEffect(() => {
-      const fetchCursos = async () => {
-        setLoading(true);
-        try {
-          const res = await fetch(`/api/admin/inscricoes/${editalId}`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`,
-            },
-          });
-  
-          const result = await res.json();
-  
-          if (!res.ok) {
-            verifyStatusRequest(res.status, result);
-            throw new Error(`Erro ao buscar inscrições: ${res.status} ${res.statusText}`);
-          }
-  
-          setInscricoes(result.data);
-        } catch (error) {
-          
-          setInscricoes([]);
-          throw new Error(`Erro ao buscar inscrições: ${error}`)
-        } finally {
-          setLoading(false);
+    const fetchCursos = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/admin/inscricoes/${editalId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+
+        const result = await res.json();
+
+        if (!res.ok) {
+          verifyStatusRequest(res.status, result);
+          throw new Error(`Erro ao buscar inscrições: ${res.status} ${res.statusText}`);
         }
-      };
-     fetchCursos();
-    }, [needUpdate]);
+
+        setInscricoes(result.data);
+      } catch (error) {
+
+        setInscricoes([]);
+        throw new Error(`Erro ao buscar inscrições: ${error}`)
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCursos();
+  }, [needUpdate]);
 
 
   if (hasPermissionForEdital('visualizar-inscricoes', editalId) || isSuperAdmin())
     return (
-      <div>
-        <h1 className="text-2xl font-bold mb-4">Gerenciamento de Inscrições</h1>
-        <div className="flex gap-4 mb-4">
-          <StatsCard title={"Nº de Inscrições"} quant={1000}>
-            <FileText className="text-[var(--admin-stats-card-text)] absolute top-4 right-4" />
-          </StatsCard>
-        </div>
-        <MainTable data = {inscricoes} columns = {columns} title={"Inscrições"}/>
-      </div>
+      <>
+        {loading && <LoaderPages />}
+        {!loading &&
+          <div>
+            <h1 className="text-2xl font-bold mb-4">Gerenciamento de Inscrições</h1>
+            <div className="flex gap-4 mb-4">
+              <StatsCard title={"Nº de Inscrições"} quant={inscricoes.length}>
+                <FileText className="text-[var(--admin-stats-card-text)] absolute top-4 right-4" />
+              </StatsCard>
+            </div>
+            <MainTable data={inscricoes} columns={columns} title={"Inscrições"} setNeedUpdate={setNeedUpdate} />
+          </div>
+        }
+      </>
     )
   else {
     return (
