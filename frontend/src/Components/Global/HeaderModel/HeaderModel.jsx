9289e8b7286df543f334e2fb43e410a6748692d3
Lean <duraesleandro21@gmail.com>
2025-09-04 14:01:29 -0300

    ♻️refactor: finalização da estilização do headermodel

--- frontend/src/Components/Global/HeaderModel/HeaderModel.jsx
+++ frontend/src/Components/Global/HeaderModel/HeaderModel.jsx
@@ -12,50 +12,50 @@ export default function HeaderModel({ headerid, children }){
 
     return(
             <header id={headerid}>
-                    <Navbar fluid rounded>
+                <Navbar fluid rounded>
 
-                        <NavbarBrand href="#">
-                            <img src="/img/logo_cead_bg_white_full.png" alt="Logo CEAD" id="img-logo"/>
-                        </NavbarBrand>
+                    <NavbarBrand href="#">
+                        <img src="/img/logo_cead_bg_white_full.png" alt="Logo CEAD" id="img-logo"/>
+                    </NavbarBrand>
 
-                        <div id="nav-item">
+                    <div id="nav-item">
+                        { children }
+                    </div>
+
+
+                    <div id="desktop-buttons-div">
+                        <ThemeToggleBtn />
+                        <UserDropdown/>
+                    </div>
+
+                    <div id="mobile-buttons-div">
+                        <ThemeToggleBtn />
+                        <UserDropdown/>
+                        <NavbarToggle className="ml-1 navbar-toggle"/>
+                    </div>
+
+                    <NavbarCollapse className="md:hidden">
+                        <div id="menu-responsive">
                             { children }
                         </div>
+                    </NavbarCollapse>
 
 
-                        <div id="desktop-buttons-div">
-                            <ThemeToggleBtn />
-                            <UserDropdown/>
-                        </div>
-
-                        <div id="mobile-buttons-div">
-                            <ThemeToggleBtn />
-                            <UserDropdown/>
-                            <NavbarToggle className="ml-1 navbar-toggle"/>
-                        </div>
-
-                        <NavbarCollapse className="md:hidden">
-                            <div id="menu-responsive">
-                                { children }
-                            </div>
-                        </NavbarCollapse>
-
-
-                    </Navbar>
-                    <div className='flex flex-row-reverse min-w-[310px] max-w-screen-2xl mt-2 text-black'>
-                        {
-                        remainingTime !== null && remainingTime !== 0 &&
-                        <p className="text-sm my-1">Sua sessão expira em {remainingTime} min.</p>
-                        }
-                        {
-                        remainingTime === 0 &&
-                        <p className="text-sm my-1">Sua sessão expirou!</p>
-                        }
-                        {
-                        remainingTime === null &&
-                        <p className="text-sm my-1">&nbsp;</p>
-                        }
-                    </div>
+                </Navbar>
+                <div className='flex flex-row-reverse max-w-screen-xl mx-auto mt-2 text-black'>
+                    {
+                    remainingTime !== null && remainingTime !== 0 &&
+                    <p className="text-sm my-1 w-full text-end mr-5">Sua sessão expira em {remainingTime} min.</p>
+                    }
+                    {
+                    remainingTime === 0 &&
+                    <p className="text-sm my-1 w-full text-end mr-5">Sua sessão expirou!</p>
+                    }
+                    {
+                    remainingTime === null &&
+                    <p className="text-sm my-1 w-full text-end mr-5">&nbsp;</p>
+                    }
+                </div>
             </header>
     );
 
